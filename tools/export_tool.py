849eb925161f11e01e0ecb432d76198b3f701cf6
mrtigermeat <tyler@tigermeat.xyz>
2026-03-01 16:06:57 -0600

    Add HTK Exporter

--- tools/export_tool.py
+++ tools/export_tool.py
@@ -1,6 +1,7 @@
 import pathlib
 import textgrid
 
+HTK_PAD_VAL = 10000000
 
 class Exporter:
     def __init__(self, predictions, output_folder=None):
@@ -31,6 +32,38 @@ class Exporter:
             tg_path.parent.mkdir(parents=True, exist_ok=True)
             tg.write(tg_path)
 
+    def save_htk(self):
+        print("Saving HTK Labels...")
+        w_out, ph_out = "", ""
+        for wav_path, wav_length, words in self.predictions:
+            wav_path = pathlib.Path(wav_path)
+
+            for word in words:
+                w_start = int(float(word.start) * HTK_PAD_VAL)
+                w_end = int(float(word.end) * HTK_PAD_VAL)
+                w_out += f"{w_start} {w_end} {word.text}\n"
+                for phoneme in word.phonemes:
+                    ph_start = int(float(phoneme.start) * HTK_PAD_VAL)
+                    ph_end = int(float(phoneme.end) * HTK_PAD_VAL)
+                    ph_out += f"{ph_start} {ph_end} {phoneme.text}\n"
+
+            if self.output_folder is not None:
+                htk_ph_path = self.output_folder / "HTK" / "Phonemes" / wav_path.with_suffix(".lab").name
+                htk_w_path = self.output_folder / "HTK" / "Words" / wav_path.with_suffix(".lab").name
+            else:
+                htk_ph_path = wav_path.parent / "HTK" / "Phonemes" / wav_path.with_suffix(".lab").name
+                htk_w_path = wav_path.parent / "HTK" / "Words" / wav_path.with_suffix(".lab").name
+
+            htk_ph_path.parent.mkdir(parents=True, exist_ok=True)
+            htk_w_path.parent.mkdir(parents=True, exist_ok=True)
+
+            with open(htk_ph_path, 'w', encoding='utf-8') as pho:
+                pho.write(ph_out)
+            with open(htk_w_path, 'w', encoding='utf-8') as wo:
+                wo.write(w_out)
+
     def export(self, out_formats):
         if 'textgrid' in out_formats:
             self.save_textgrids()
+        if 'htk' in out_formats:
+            self.save_htk()
